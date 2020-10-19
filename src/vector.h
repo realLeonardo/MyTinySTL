1908cb4a754a2ae16c2c5d68aa8aedeb02c3f36b
Leonardo <imrealleonardo@gmail.com>
2020-10-19 16:40:17 +0800

    -format vector code;

--- src/vector.h
+++ src/vector.h
@@ -3,8 +3,8 @@
  * Need Achieve: Random access,
  * Methods:
  * * void push_back(T), int size()
- *
- */
+ * * TODO
+*/
 #ifndef MYTINYSTL_VECTOR_H
 #define MYTINYSTL_VECTOR_H
 
@@ -15,124 +15,130 @@ namespace tiny_stl{
 template <typename T>
 class vector{
 public:
-    typedef tiny_stl::allocator<T> data_allocator;
+  typedef tiny_stl::allocator<T> data_allocator;
+
 public:
-    enum {DEFAULT_CAPACITY = 16};
-    // constructors
-    vector() {
-        init(DEFAULT_CAPACITY);
-    }
-    explicit vector(size_t cap) {
-        init(cap);
-    }
-    // NOTE: copy constructor
-    // vector(vector& v);
-    ~vector();
+  enum {DEFAULT_CAPACITY = 16};
+  // Default ctor
+  vector() {
+    init(DEFAULT_CAPACITY);
+  }
+  explicit vector(const size_t cap) {
+    init(cap);
+  }
+  // Copy ctor
+  vector(const vector& v);
+  // dtor
+  ~vector();
 
-    T operator[](const int index);
-    // TODO
-    // vector& operator=(const vector& v);
+  T operator[](const int index);
+  // Copy assignment operator
+  vector& operator=(const vector& v);
 
-    void push_back(T t);
-    void push_back(T& t);
-    int size() const;
-    int capacity() const;
+  void push_back(const T& t);
+  const int size() const;
+  const int capacity() const;
 
-    // iterator
-    T* begin();
-    T* end();
+  // iterator
+  T* begin();
+  T* end();
 
 private:
-    size_t _size = 0;
-    size_t _cap = 0;
-    T* _begin;
-    T* _end;
+  size_t _size = 0;
+  size_t _cap = 0;
+  T* _begin;
+  T* _end;
 
-    // 构造函数相关
-    void init(size_t cap = -1);
+  // 构造函数相关
+  void init(size_t cap = -1);
 };
 
 // 构造函数通用函数
 template <typename T>
 void vector<T>::init(size_t cap) {
-    if (cap < 0) {
-        cap = DEFAULT_CAPACITY;
-    }
+  if (cap < 0) {
+    cap = DEFAULT_CAPACITY;
+  }
 
-    _begin = data_allocator::allocate(cap);
-    _size = 0;
-    _cap = cap;
+  _begin = data_allocator::allocate(cap);
+  _size = 0;
+  _cap = cap;
+}
+
+template <typename T>
+vector<T>::vector(const vector<T> &v) {
+  _cap = v._cap;
+  _size = v._size;
+
+  _begin = data_allocator::allocate(_cap);
+  for (size_t i=0; i<_size; i++) {
+    _begin[i] = v[i];
+  }
+}
+
+template <typename T>
+vector<T>& vector<T>::operator=(const vector<T> &v) {
+  _cap = v._cap;
+  _size = v._size;
+
+  _begin = data_allocator::allocate(_cap);
+  for (size_t i=0; i<_size; i++) {
+    _begin[i] = v[i];
+  }
+
+  return *this;
 }
 
 template <typename T>
 vector<T>::~vector() {
-    data_allocator::deallocate(_begin, _cap);
-    _end = nullptr;
+  data_allocator::deallocate(_begin, _cap);
+  _end = nullptr;
 }
 
 template <typename T>
 T vector<T>::operator[](const int index) {
-    return _begin[index];
+  return _begin[index];
 }
 
 template <typename T>
-void vector<T>::push_back(T t) {
-    if (_size >= _cap) {
-        _cap += DEFAULT_CAPACITY;
-        // reallocate memory
-        T* temp = data_allocator::allocate(_cap);
+void vector<T>::push_back(const T& t) {
+  if (_size >= _cap) {
+    _cap += DEFAULT_CAPACITY;
+    // reallocate memory
+    T* temp = data_allocator::allocate(_cap);
 
-        for (int i=0; i<_size; i++) {
-            temp[i] = _begin[i];
-        }
-
-        data_allocator::deallocate(_begin, _cap);
-        _begin = temp;
+    for (int i=0; i<_size; i++) {
+      temp[i] = _begin[i];
     }
 
-    _begin[_size] = t;
-    _size++;
+    data_allocator::deallocate(_begin, _cap);
+    _begin = temp;
+  }
+
+  _begin[_size] = t;
+  _size++;
 }
 
 template <typename T>
-void vector<T>::push_back(T& t) {
-    if (_size >= _cap) {
-        _cap += DEFAULT_CAPACITY;
-        // reallocate memory
-        T* temp = data_allocator::allocate(_cap);
-
-        for (int i=0; i<_size; i++) {
-            temp[i] = _begin[i];
-        }
-
-        data_allocator::deallocate(_begin, _cap);
-        _begin = temp;
-    }
-
-    _begin[_size] = t;
-    _size++;
-}
-
-
-template <typename T>
-int vector<T>::size() const {
-    return _size;
+const int vector<T>::size() const {
+  return _size;
 }
 
 template <typename T>
-int vector<T>::capacity() const {
-    return _cap;
+const int vector<T>::capacity() const {
+  return _cap;
 }
 
 template <typename T>
 T* vector<T>::begin() {
-    return _begin;
+  return _begin;
 }
 
 template <typename T>
 T* vector<T>::end() {
-    return _begin + _size;
+  return _begin + _size;
 }
+
 }
 
 #endif //MYTINYSTL_VECTOR_H
--- test/vector_test.h
+++ test/vector_test.h
@@ -5,26 +5,30 @@
 #include <iostream>
 
 namespace tiny_stl_test{
-    using tiny_stl::vector;
-    using std::cout;
-    using std::endl;
+using tiny_stl::vector;
+using std::cout;
+using std::endl;
 
-    void vector_test() {
-        vector<int> v(3);
+void vector_test() {
+  vector<int> v(10);
+  int x = 5;
+  int& y = x;
 
-        v.push_back(1);
-        v.push_back(2);
-        v.push_back(3);
-        v.push_back(4);
+  v.push_back(1);
+  v.push_back(2);
+  v.push_back(3);
+  v.push_back(4);
+  v.push_back(x);
+  v.push_back(y);
 
-        for(auto i=v.begin(); i<v.end(); i++){
-            cout << *i << endl;
-        }
+  for(auto i=v.begin(); i<v.end(); i++){
+    cout << *i << endl;
+  }
 
-        cout << v.size() << endl;
-        cout << v.capacity() << endl;
+  cout << "size: " << v.size() << endl;
+  cout << "capacity: " << v.capacity() << endl;
+}
 
-    }
 }
 
 #endif //MYTINYSTL_VECTOR_TEST_H
